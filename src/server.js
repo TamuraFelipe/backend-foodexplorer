5a6eb46f4a88548ccd28ebd933fd6d1a1c6a904f
Tamura <tamurafelipe@gmail.com>
2023-06-01 10:53:22 -0300

    Depe dotenv

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors");
+require("dotenv/config");
 
 const database = require("./database/sqlite");
 
@@ -34,5 +35,5 @@ app.use(( error, request, response, next ) => {
     })
 })
 
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
